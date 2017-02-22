b4c6ef90d4a390aecb1adff0eace5ecd91128cdd
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2017-02-22 15:44:25 +0100

    Add .. something

---
+++ src/hash2017/AssignmentParser.java
@@ -0,0 +1,10 @@
+package hash2017;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AssignmentParser {
+	public static void parseFile(File file) throws IOException {
+		
+	}
+}
--- src/hash2017/Main.java
+++ src/hash2017/Main.java
@@ -1,10 +1,26 @@
 package hash2017;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Team FH* Reloaded");
-
+		String[] fileNames = {
+				"file1.in", 
+				"file2.in",
+				"file3.in"
+			};
+		
+		for (String fileName : fileNames) {
+			solveTaskUsingInput(new File(fileName));
+		}
+		
+	}
+	
+	public static void solveTaskUsingInput(File file) throws IOException {
+		AssignmentParser.parseFile(file);
 	}
 
 }
