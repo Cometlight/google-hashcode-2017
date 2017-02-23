8410abc4e838a33c9a9605289c4c381550b88554
daniel <dgr7348@students.fhv.at>
2017-02-23 20:30:05 +0100

    junit

--- src/hash2017/AssignmentParser.java
+++ src/hash2017/AssignmentParser.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.util.LinkedList;
 import java.util.List;
 
+import hash2017.model.Cache;
 import hash2017.model.Endpoint;
 import hash2017.model.Structure;
 import hash2017.model.Video;
@@ -14,42 +15,48 @@ import hash2017.model.Video;
 public class AssignmentParser {
 	public static Structure parseFile(File file) throws IOException {
 		System.out.print(file.getName() + "\n");
-		
-		//pares into tokens
+
+		// pares into tokens
 		Charset charset = Charset.forName("US-ASCII");
-		List<String> content = Files.readAllLines(file.toPath(),charset );
-		
-		//parse dims
+		List<String> content = Files.readAllLines(file.toPath(), charset);
+
+		// parse dims
 		List<Integer> dims = GetTokens(content.get(0));
 		int videosCount = dims.get(0);
 		int endpointCount = dims.get(1);
 		int requestDescriptions = dims.get(2);
-		int caches = dims.get(3);
+		int cachesCount = dims.get(3);
 		int cacheSize = dims.get(4);
-	
+
 		// videos
 		Video[] videos = new Video[videosCount];
 		List<Integer> videoSizes = GetTokens(content.get(1));
-		for(int i = 0; i < videosCount; ++i){
+		for (int i = 0; i < videosCount; ++i) {
 			videos[i] = new Video(i, videoSizes.get(i));
 		}
-		
-		Endpoint[] endpoints = new Endpoint[endpointCount];  
-		
-		return new Structure(null,null,videos);
+
+		// caches
+		Cache[] caches = new Cache[cachesCount];
+		for (int i = 0; i < cachesCount; ++i) {
+			caches[i] = new Cache(i, cacheSize);
+		}
+
+		Endpoint[] endpoints = new Endpoint[endpointCount];
+
+		return new Structure(caches, null, videos);
 	}
-	
+
 	public static List<Integer> GetTokens(String line) {
 		String[] split = line.split("\\s+");
-	
+
 		List<Integer> result = new LinkedList<Integer>();
-		
-		for(int i = 0; i < split.length; ++i) {
+
+		for (int i = 0; i < split.length; ++i) {
 			result.add(Integer.parseInt(split[i]));
 		}
-		
+
 		return result;
-		
+
 	}
-	
+
 }
