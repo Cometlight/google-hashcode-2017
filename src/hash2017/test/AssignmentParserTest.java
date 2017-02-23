42a5d5d033ffd187fbdf7d7140324540f72e76b3
Norskan <simon.angerer@hotmail.com>
2017-02-23 21:37:19 +0100

    renaming

--- src/hash2017/test/AssignmentParserTest.java
+++ src/hash2017/test/AssignmentParserTest.java
@@ -12,7 +12,7 @@ import hash2017.model.*;
 public class AssignmentParserTest {
 
 	@Test
-	public void test() throws IOException {
+	public void kitten() throws IOException {
 		Structure testStructure = AssignmentParser.parseFile(new File("kittens.in"));
 		assertNotNull("structure not null", testStructure);
 		assertTrue("caches exist", testStructure.caches.length > 0);
@@ -24,7 +24,7 @@ public class AssignmentParserTest {
 	}
 	
 	@Test
-	public void test1() throws IOException {
+	public void meAtTheZoo() throws IOException {
 		Structure testStructure = AssignmentParser.parseFile(new File("me_at_the_zoo.in"));
 		assertNotNull("structure not null", testStructure);
 		assertTrue("caches exist", testStructure.caches.length > 0);
@@ -36,7 +36,7 @@ public class AssignmentParserTest {
 	}
 	
 	@Test
-	public void test2() throws IOException {
+	public void trendingToday() throws IOException {
 		Structure testStructure = AssignmentParser.parseFile(new File("trending_today.in"));
 		assertNotNull("structure not null", testStructure);
 		assertTrue("caches exist", testStructure.caches.length > 0);
@@ -48,7 +48,7 @@ public class AssignmentParserTest {
 	}
 	
 	@Test
-	public void test3() throws IOException {
+	public void VideosWorthSpreading() throws IOException {
 		Structure testStructure = AssignmentParser.parseFile(new File("videos_worth_spreading.in"));
 		assertNotNull("structure not null", testStructure);
 		assertTrue("caches exist", testStructure.caches.length > 0);
