997e44db67536e59a95ae8c0c617d6039f757ee9
daniel <dgr7348@students.fhv.at>
2017-02-23 19:35:12 +0100

    model attributes

--- src/hash2017/model/Video.java
+++ src/hash2017/model/Video.java
@@ -1,9 +1,12 @@
 package hash2017.model;
 
 public class Video {
+	public int id;
+	public int size;
 
-	public Video() {
-		// TODO Auto-generated constructor stub
+	public Video(int id, int size) {
+		this.id = id;
+		this.size = size;
 	}
 
 }
