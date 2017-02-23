f618b61408adf1289b0e214199ecb63b1df9ce7f
daniel <dgr7348@students.fhv.at>
2017-02-23 22:15:52 +0100

    junit

--- src/hash2017/test/AssignmentParserTest.java
+++ src/hash2017/test/AssignmentParserTest.java
@@ -1,5 +1,7 @@
 package hash2017.test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
@@ -7,7 +9,7 @@ import java.io.IOException;
 import org.junit.Test;
 
 import hash2017.AssignmentParser;
-import hash2017.model.*;
+import hash2017.model.Structure;
 
 public class AssignmentParserTest {
 
@@ -21,6 +23,12 @@ public class AssignmentParserTest {
 		assertTrue("all caches loaded", testStructure.caches[testStructure.caches.length-1].capacity > 0);
 		assertTrue("all endpoints loaded", testStructure.endpoints[testStructure.endpoints.length-1].datacenterLatency > 0);
 		assertTrue("all videos loaded", testStructure.videos[testStructure.videos.length-1].size > 0);
+		assertEquals("check first endpoint value", testStructure.endpoints[0].datacenterLatency, 1645);
+		assertEquals("check first video value", testStructure.videos[0].size, 936);
+		assertEquals("check last endpoint value", testStructure.endpoints[testStructure.endpoints.length-1].datacenterLatency, 2157);
+		assertEquals("check last video value", testStructure.videos[testStructure.videos.length-1].size, 764);
+		assertEquals("check last requests value", testStructure.endpoints[308].videoRequests.get(testStructure.videos[7055]).intValue(), 1947);
+		assertEquals("check first requests value", testStructure.endpoints[781].videoRequests.get(testStructure.videos[9031]).intValue(), 8367);
 	}
 	
 	@Test
